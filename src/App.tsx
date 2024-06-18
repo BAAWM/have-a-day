69878a3d68388905176b6c324d7d50f3da76f042
BAAWM <rellimred@gmail.com>
2024-06-18 10:15:59 +0100

    Your commit message

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import grafImage from './assets/graf.jpg';
+
+function Box() {
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Hello</h1>
+        <img src={grafImage} alt="logo" />
+      </header>
+    </div>
+  )}
+export default Box;
